aec18bc29d8cada4e9d8879d0a8a2c9ab5e421f4
ak804670 <ak804670@gmail.com>
2023-11-25 23:30:46 +0530

    component folder added

--- catfilxfronted/src/App.js
+++ catfilxfronted/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Navber from './components/Navber';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div className="App">
-      catfilx
+      <Navber/>
     </div>
   );
 }
